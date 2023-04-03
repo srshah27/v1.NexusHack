2c51a4179ae1455584092e1d8b1a1eefe4b7639a
srshah27 <snehilshah27@gmail.com>
2023-04-03 14:50:38 +0530

    fixed horizontal scroll at mobile layouts

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import "../styles/neon.css";
 import "../styles/components/header.css";
 import "../styles/components/hero.css";
+import "../styles/components/about.css";
 import "../styles/utils.css";
